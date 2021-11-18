ac6724a97fc8862e5364970c1e2986be42025340
jovangeorge27 <91710503+jovangeorge27@users.noreply.github.com>
2021-11-17 16:31:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,81 @@
-#   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
-wn.addshape(apple_image) # Make the screen aware of the new file
+wn.addshape(apple_image)
 wn.bgpic("background.gif")
-apple = trtl.Turtle()
-drawer = trtl.Turtle()
+
+letters = ["A", "B", "C", "D", "E"]
+
+
+apple_list = []
+apple_letters = []
+
+for i in range (5):
+  apple_list.append(trtl.Turtle())
+  apple_letters.append(rand.choice(letters))
+
 #-----functions-----
-# given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(apple_image)
+def draw_apple(index):
+  apple_list[index].penup()
+  apple_list[index].shape(apple_image)
+  wn.tracer(False)
+  apple_list[index].setx(rand.randint(-150,150))
+  apple_list[index].sety(rand.randint(-15,100))
+  apple_list[index].sety(apple_list[index].ycor()-30)
+  apple_list[index].color("blue")
+  apple_list[index].write(apple_letters[index], align = "center", font=("Courier", 25, "bold")) 
+  apple_list[index].sety(apple_list[index].ycor()+30)
+  apple_list[index].showturtle()
+  wn.tracer(True)
   wn.update()
-def drop_apple():
-  apple.penup()
-  drawer.clear()
-  drawer.hideturtle()
-  apple.goto(0,-130)
-  
-def draw_an_A():
-  drawer.goto(-10,-30)
-  drawer.color("white")
-  drawer.write("A", font=("Arial", 30, "bold"))
-  
+
+def drop_apple(index):
+  apple_list[index].penup()
+  apple_list[index].clear()
+  apple_list[index].sety(-150)
+  apple_list[index].hideturtle()
+ 
+
+def letterA():
+  for i in range (5):
+   if apple_letters[i] == "A":
+    drop_apple(i)
+
+def letterB():
+  for i in range (5):
+   if apple_letters[i] == "B":
+    drop_apple(i)
+
+def letterC():
+  for i in range (5):
+   if apple_letters[i] == "C":
+    drop_apple(i)
+
+def letterD():
+  for i in range (5):
+   if apple_letters[i] == "D":
+    drop_apple(i)
+
+def letterE():
+  for i in range (5):
+   if apple_letters[i] == "E":
+    drop_apple(i)
 
 #-----function calls-----
-draw_apple(apple)
-draw_an_A()
-wn.onkeypress(drop_apple, "a")
+for i in range (5):
+  draw_apple(i)
+
+wn.onkeypress(letterA, "a")
+wn.onkeypress(letterB, "b")
+wn.onkeypress(letterC, "c")
+wn.onkeypress(letterD, "d")
+wn.onkeypress(letterE, "e")
+
 wn.listen()
 wn.mainloop()
-    
 
